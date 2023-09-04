3ab6a567477bb37b2ffba87f0ebed6583a463df2
vad1msa4 <vadimsa4enko@gmail.com>
2023-09-04 15:57:57 +0300

    deplyment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,8 @@ const config = {
   projectName: 'TechWriteIt', // Usually your repo name.
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  deploymentBranch: "gh-pages",
+  deploymentBranch: 'gh-pages',
+  trailingSlash: true,
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
