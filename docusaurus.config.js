88f04908bd4a87c5e0d167f2168f27c75c317f29
vad1msa4 <vadimsa4enko@gmail.com>
2023-09-05 16:41:55 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,7 +86,7 @@ const config = {
             items: [
               {
                 label: 'Docs',
-                to: '/docs/intro',
+                to: '/docs/about-techwriteit/',
               },
             ],
           },
