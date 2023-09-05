c070860ca65f87a21b636c99b66b7746c5e12e44
vad1msa4 <vadimsa4enko@gmail.com>
2023-09-05 16:47:14 +0300

    Deploy website - based on 06165fc6f647eeacb4f6104dc061c6d7fe4125f7

---
+++ assets/js/9e4b4a1b.5f2aaf1d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunktech_write_it=self.webpackChunktech_write_it||[]).push([[767],{3303:e=>{e.exports=JSON.parse('{"permalink":"/TechWRiteIt/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
